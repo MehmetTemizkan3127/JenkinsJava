a93ff84368707bb76015f272c2662d8a78bf6320
Mehmet <mehmettemizkanabs@gmail.com>
2025-05-30 11:57:47 +0200

    v2

--- src/C03.java
+++ src/C03.java
@@ -2,8 +2,9 @@ public class C03 {
     public static void main(String[] args) {
         int sayi1 = 12;
         int sayi2 = 8;
+        int sayi3 = 5;
 
         int toplam = sayi1 + sayi2;
-        System.out.println("Sayilarin toplami = "+sayi1 + " + " + sayi2 + " = " + toplam);
+        System.out.println("Sayilarin toplami = " + toplam);
     }
 }
